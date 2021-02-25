e927062ad502832ca9bff750223e4425cdfe77f9
tharlys <tharlysalves@grupobrisanet.com.br>
2021-02-24 21:27:09 -0300

    feat: :sparkles: create repository custom
    Criado repositorio de usuario constumizado para os usuários

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,16 +1,16 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
   async create(request: Request, response: Response) {
     let { name, email } = request.body;
 
-    name = name.toUpperCase();
-    email = email.toLowerCase().trim();
+    name = name?.toUpperCase();
+    email = email?.toLowerCase().trim();
 
-    // extencion repository typeorm
-    const userRepository = getRepository(User);
+    // extencion custom repository
+    const userRepository = getCustomRepository(UsersRepository);
 
     // search user fur email
     const userAlreadyExists = await userRepository.findOne({ email });

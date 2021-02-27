7a25c7e852fe2cacdc30859d36bfb7d22f08a37b
tharlys <tharlysalves@grupobrisanet.com.br>
2021-02-27 16:44:12 -0300

    feat: :sparkles: logs mode dev
    implementado os logs apenas no modo de desenvolvemento

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -7,9 +7,13 @@ export default async (): Promise<Connection> => {
   // create connection database
   return createConnection(
     Object.assign(defaultOptions, {
-      database: process.env.NODE_ENV === 'test' 
-        ? './src/database/database.test.sqlite' 
-        : defaultOptions.database
+      database: process.env.NODE_ENV === 'test'
+        ? './src/database/database.test.sqlite'
+        : defaultOptions.database,
+
+      logging: process.env.NODE_ENV === 'dev'
+        ? true
+        : false
     })
   );
 }
